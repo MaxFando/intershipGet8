c9af87bffd2f3c085d8b92b07fe6c395e4b750b2
Max Fando <fando97@mail.ru>
2018-08-30 14:33:29 +0300

    Задание 3. Обработка массива
    Способ воода http://localhost:8080/index.php?to_bd=YES&keys=test,testing&values=rest,framework

--- public/api/index.php
+++ public/api/index.php
@@ -1,1 +1,28 @@
-<?php phpinfo(); ?>
+<?php 
+ $host    = 'db:3306';
+ $user    = 'root';
+ $pass    = 'qwerty123';
+ $db_name = 'Test';
+
+ $link = new mysqli($host, $user, $pass, $db_name);
+
+ if (!$link) {
+     die('Ошибка подключения (' . mysqli_connect_errno() . ') '
+             . mysqli_connect_error());
+ }
+
+ if($_REQUEST['to_bd'] === 'YES') {
+     $keys = explode(',', $_GET['keys']);
+     $values = explode(',', $_GET['values']);
+
+     for ($i = 0; $i < count($keys); $i++) {
+         mysqli_query($link, "INSERT INTO `Test` (`basic`, `value`) VALUES ('$keys[$i]', '$values[$i]')");
+     }
+     echo 'Выполнено ';
+     print_r($_REQUEST);
+ }
+
+ mysqli_close($link);
+
+
+?>
